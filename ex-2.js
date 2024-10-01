07d420de35a96eba17d80a00d2a08457c524a5a2
Eisss20 <pajenan.j@gmail.com>
2024-10-01 13:46:51 +0700

    Feat : Answer Object

--- ex-2.js
+++ ex-2.js
@@ -6,5 +6,19 @@ const product = {
   ratings: 2.5,
   photo: null,
 };
+
 let socialMediaShare = "facebookShare";
-// Start code here
+
+product["ratings"] = 4.5;
+
+product ["photo"] = "https://i01.appmifile.com/webfile/globalimg/products/pc/mi-air-purifier-3H/replace_03.jpg";
+
+product.product ="IT0101010"
+
+product["price"] = 6000;
+
+delete product.ratings;
+
+product[socialMediaShare]=45.5;
+
+console.log(product);
